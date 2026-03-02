d4d5a4dabce144f3d616f13af3760cc68fffcd4f
FengSheng_Hust <3114334825@qq.com>
2026-03-02 16:03:23 +0800

    完成‘随机过程试卷.md’的适配，主要修复：list中的换行和间距，增加image无法找到的提示，修复katex渲染不等号失败的bug，修复编号未能继续的bug

--- src/markpress/renders/image.py
+++ src/markpress/renders/image.py
@@ -1,18 +1,45 @@
 import os
-from reportlab.platypus import Image, Spacer
+from reportlab.platypus import Image, Spacer,Paragraph
 from reportlab.lib.units import mm
 from .base import BaseRenderer
+from reportlab.lib import colors
+from reportlab.lib.enums import TA_LEFT, TA_CENTER, TA_RIGHT, TA_JUSTIFY
+from reportlab.lib.styles import ParagraphStyle
 
 
 class ImageRenderer(BaseRenderer):
-    """图片渲染器"""
+
+    def __init__(self, config, stylesheet):
+        super().__init__(config, stylesheet)
+        self._init_paragraph_style()
+
+    def _init_paragraph_style(self):
+        # 普通的段落样式
+        if "Body_Text" not in self.styles:
+            conf = self.config.styles.body
+            align_map = {'LEFT': TA_LEFT, 'CENTER': TA_CENTER, 'RIGHT': TA_RIGHT, 'JUSTIFY': TA_JUSTIFY}
+
+            self.styles.add(ParagraphStyle(
+                name="Body_Text",
+                fontName=self.config.fonts.regular,
+                fontSize=conf.font_size,
+                leading=conf.leading,
+                alignment=align_map.get(conf.alignment, TA_JUSTIFY),
+                spaceAfter=conf.space_after,
+                textColor=colors.HexColor(self.config.colors.text_primary),
+                wordWrap='CJK',
+                splitLongWords=True,
+                keepWithNext=False
+            ))
+
+    # 图片渲染器
     def render(self, image_path: str, alt_text: str = "", **kwargs):
         # 获取可用宽度
         avail_width = kwargs.get('avail_width', 160 * mm)
         # 检查图片文件是否存在
         if not os.path.exists(image_path):
-            print(f"警告: 图片文件不存在: {image_path}")
-            return []
+            print(f"警告: 图片文件不存在或无法访问: {image_path}")
+            return [Paragraph(f"<b><font color='red'>加载图片{alt_text}失败</font></b>",self.styles["Body_Text"])]
         try:
             img = Image(image_path)
             # 获取原始尺寸
